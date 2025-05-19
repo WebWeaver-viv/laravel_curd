fc7ef89c7f7dcea831014f151fca1ba1c4bcc323
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-05-19 17:09:08 +0530

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="bg-dark py-3">
+
+        <h3 class="text-white text-center">Home</h3>
+    </div>
+    <div class="container">
+      <div class="row justify-content-center mt-4 my-4">
+        
+        <div class="col-md-10 d-flex justify-content-end ">
+            <a href="{{ route('products.index') }}" class="btn btn-dark">Product</a> 
+
+        </div>
+            <div class="container ">
+                <div>
+                    <img src="" alt="">
+                </div>
+                <div class="">
+                    <h4>product Name </h4>
+
+                </div>
+
+            </div>
+        
+
+    
+</body>
+
+</html>
